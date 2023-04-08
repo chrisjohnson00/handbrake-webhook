52c9ff523cf62059f88a78eef0edf5d23b7f321c
Chris Johnson <chrisjohnson00@hotmail.com>
2023-04-08 10:24:28 -0700

    fix: improper call of constructor

--- utilities/skip.py
+++ utilities/skip.py
@@ -3,8 +3,9 @@ from utilities.config import ConfigFileReader
 
 
 def skip_send(file_path):
-    if utilities.env_config.get_config_with_default("SKIP_CONFIG_FILE", False):
-        skip_config = ConfigFileReader().get_config()
+    config_file = utilities.env_config.get_config_with_default("SKIP_CONFIG_FILE", False)
+    if config_file:
+        skip_config = ConfigFileReader(config_file).get_config()
         return should_skip(file_path, skip_config)
     else:
         return False
